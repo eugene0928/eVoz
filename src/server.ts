fef3477b0482f719c9ff0bff706da7a15bbdb524
eugene0928 <umid09tuxtayev.com>
2022-07-20 01:54:34 +0500

    db loading

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Express, Router } from "express";
-import { AppDataSource } from './utils/data-source.js'
-import routes from "./modules/index.js"
+import { AppDataSource } from './utils/data-source.js';
+import loadData from "./data/podcast.js";
 
 const app: Express = express();
 const PORT:number = 4000;
@@ -10,12 +10,13 @@ const PORT:number = 4000;
         const db = await AppDataSource.initialize()
         console.log("Db is initialized!")
 
+        await loadData(AppDataSource)
         // middlewares
         app.use(express.json())
         
         // routes
-        app.use(routes.Reg_Router)
-   } catch (error: any) {
+
+    } catch (error: any) {
        console.log(error.message) 
    }
 
