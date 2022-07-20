883356eecafbac4af3f007e45b077ea55e5a3356
eugene0928 <umid09tuxtayev.com>
2022-07-20 23:15:18 +0500

    admin changed to user

---
+++ src/entity/user.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity, Unique, CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from 'typeorm'
+import { IsEmail, Length } from 'class-validator'
+
+@Entity()
+@Unique(["email"])
+export class Users extends BaseEntity{
+    @PrimaryGeneratedColumn("uuid")
+    id!: string
+
+    @Column()
+    @Length(2, 32)
+    name!: string
+
+    @Column({ name: "email" })
+    @IsEmail()
+    email!: string
+
+    @Column()
+    @Length(8, 16)
+    password!: string
+
+    @Column("boolean", { default: false })
+    is_admin!: boolean
+ 
+    @CreateDateColumn()
+    created_at!: Date
+
+    @UpdateDateColumn()
+    updated_at!: Date
+
+    @DeleteDateColumn()
+    deleted_at!: Date
+}
