a2f617477362f9f878006604b108ebb4508876df
eugene0928 <umid09tuxtayev.com>
2022-07-22 01:53:52 +0500

    updates

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,26 @@
 import express, { Express } from "express";
 import { AppDataSource } from './utils/data-source.js';
 import loadData from "./data/podcast.js";
+import Routers from "./modules/index.js";
+import { handler } from "./utils/errorHandling.js"
 
 const app: Express = express();
 const PORT:number = 4000;
 
 (async function() {
    try {
-        const db = await AppDataSource.initialize()
-        console.log("Db is initialized!")
+      const db = await AppDataSource.initialize()
+      console.log("Db is initialized!")
 
-        await loadData(AppDataSource)
-        // middlewares
-        app.use(express.json())
-        
-        // routes
+      await loadData(AppDataSource)
+      // middlewares
+      app.use(express.json())
+      
+      // routes
+      app.use(Routers.UserRouter)
 
+      //error handler
+      app.use(handler)
     } catch (error: any) {
        console.log(error.message) 
    }
