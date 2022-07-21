fe38ef3204a84c082d02775b00bbfc1d6b05d0d0
eugene0928 <umid09tuxtayev.com>
2022-07-22 03:20:14 +0500

    register is done

--- src/modules/user/controller.ts
+++ src/modules/user/controller.ts
@@ -1,10 +1,43 @@
 import { Request, Response, NextFunction } from "express";
 import { Users } from "../../entity/user.js";
 import bcrypt from "bcrypt";
-import { InternalServerError, BadRequestError } from "../../utils/custom.errors.js"
+import { InternalServerError, BadRequestError, ValidationError } from "../../utils/custom.errors.js";
+import { reg_schema } from "../../utils/joi.js";
+import { AppDataSource } from "../../utils/data-source.js";
 
-export const Register = (req: Request, res: Response, next: NextFunction) => {
+export const Register = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { error, value } = reg_schema.validate(req.body)
 
+        if(error) {
+            next(new ValidationError(422, error.message))
+            return
+        }
+
+        value.password = await bcrypt.hash(value.password, 5)
+        // insert data db
+        let data = undefined;
+        try {
+            data = await AppDataSource.createQueryBuilder()
+                            .insert()
+                            .into(Users)
+                            .values([{ 
+                                name: value.name,
+                                email: value.email,
+                                password: value.password
+                             }])
+                            .execute();
+        } catch (error: any) {
+            next(new ValidationError(422, error.message))
+            return
+        }
+        
+        console.log(data.identifiers[0].id)
+
+        res.status(200).json({ status: 200, message: "Successfully registered", data: data.identifiers[0].id })
+    } catch (error: any) {
+        next(new InternalServerError(500, error.message))
+    }
 }
 
 export const Login = async (req: Request, res: Response, next: NextFunction) => {
