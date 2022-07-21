63383c1accf17211ed5cfb5d3b5aa7bfafc3d3ad
eugene0928 <umid09tuxtayev.com>
2022-07-22 01:52:47 +0500

    error handler

---
+++ src/utils/errorHandling.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from "express";
+import { error } from "../types/error.js";
+import { writeFile } from "fs/promises";
+import { join } from "path";
+
+export const handler = async (error: error, req: Request, res: Response, next: NextFunction) => {
+    if(error.status == 500) {
+        const data = `${new Date().toISOString()}___${req.url}___${req.method}___${error.message}`;
+        writeFile(join(process.cwd(), "logger.txt"), data)
+
+        res.status(500).json({ status: 500, name: "InternalServerError", message: "Internal Server Error" })
+        return
+    }
+
+    res.status(error.status).json({ status: error.status, name: error.name, message: error.message })
+}
