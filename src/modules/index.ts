17241d192b247f0324ca944dd9766eedf032c30f
eugene0928 <umid09tuxtayev.com>
2022-07-23 23:00:18 +0500

    podcast route

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,9 @@
 import UserRouter from "./user/router.js";
 import CategoryRouter from "./category/router.js";
+import PodcastRouter from "./podcast/router.js";
 
 export default {
     UserRouter,
-    CategoryRouter
+    CategoryRouter,
+    PodcastRouter
 }
