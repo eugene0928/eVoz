2982570118a0a0cfbdbac583221aed381808e47c
eugene0928 <umid09tuxtayev.com>
2022-07-23 23:00:40 +0500

    minor bug fixed

--- src/utils/errorHandling.ts
+++ src/utils/errorHandling.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import { error } from "../types/error.js";
-import { writeFile } from "fs/promises";
+import { appendFile } from "fs/promises";
 import { join } from "path";
 
 export const handler = async (error: error, req: Request, res: Response, next: NextFunction) => {
     if(error.status == 500) {
-        const data = `${new Date().toISOString()}___${req.url}___${req.method}___${error.message}`;
-        writeFile(join(process.cwd(), "logger.txt"), data)
+        const data = `${new Date().toISOString()}___${req.url}___${req.method}___${error.message}\n`;
+        appendFile(join(process.cwd(), "logger.txt"), data)
 
         res.status(500).json({ status: 500, name: "InternalServerError", message: "Internal Server Error" })
         return
