efb40a0fc9fdbfdb491e2db4bccc32958d45dca7
eugene0928 <umid09tuxtayev.com>
2022-07-25 00:56:43 +0500

    admin route

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,9 +1,11 @@
 import UserRouter from "./user/router.js";
 import CategoryRouter from "./category/router.js";
 import PodcastRouter from "./podcast/router.js";
+import AdminRouter from "./admin/router.js";
 
 export default {
     UserRouter,
     CategoryRouter,
-    PodcastRouter
+    PodcastRouter,
+    AdminRouter
 }
