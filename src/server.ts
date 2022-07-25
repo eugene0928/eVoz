6dcd529f13d46841d9d3e965a08703e294731cc3
eugene0928 <umid09tuxtayev.com>
2022-07-25 22:50:35 +0500

    static folders

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express, { Express } from "express";
+import "./conf.js";  
+import "./bot/bot.js";
 import { AppDataSource } from './utils/data-source.js';
 import loadData from "./data/podcast.js";
 import Routers from "./modules/index.js";
 import { handler } from "./utils/errorHandling.js";
 import fileUpload from "express-fileupload";
-import "./conf.js";
+import { join } from "path";
  
 const app: Express = express();
 const PORT:number = 4000;
@@ -15,6 +17,11 @@ const PORT:number = 4000;
       console.log("Db is initialized!")
 
       await loadData(AppDataSource)
+
+      // static folders
+      app.use(express.static(join(process.cwd(), "src", "images")))
+      app.use(express.static(join(process.cwd(), "src", "podcasts")))
+
       // middlewares
       app.use(express.json())
       app.use(fileUpload()) 
